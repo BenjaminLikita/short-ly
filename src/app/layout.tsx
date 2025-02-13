49079c96eb3b1897e3e3afa1c7a8b55968aabd10
Benjamin Likita <benjaminlikita3@gmail.com>
2025-02-13 21:03:48 +0100

    initial (complete)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,16 @@
 import type { Metadata } from "next";
-import { Geist_Mono, Poppins } from "next/font/google";
+import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
 
 const poppins = Poppins({
   variable: "--font-poppins",
   subsets: ['latin', 'latin-ext'],
   weight: ['100', '200', '300', '400', '500', '600', '700', '800']
+});
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 export const geistMono = Geist_Mono({
@@ -26,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${poppins.className} antialiased`}
+        className={`${poppins.className} ${geistMono.variable} ${geistSans.variable} antialiased`}
       >
         {children}
       </body>
