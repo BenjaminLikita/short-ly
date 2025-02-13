b777f5d191271a7f39eaab9ebab49f4a0521973c
Benjamin Likita <benjaminlikita3@gmail.com>
2025-02-13 20:42:48 +0100

    initial (complete)

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,8 +1,9 @@
 import { getUrl } from "@/utils/server"
 import { redirect } from "next/navigation"
 
-const RedirectToUrl = async ({ params }:{ params: {id: string} }) => {
-  const url = await getUrl(params.id)
+const RedirectToUrl = async ({ params }:{ params: Promise<{id: string}> }) => {
+  const id = (await params).id
+  const url = await getUrl(id)
   if(!url || !url.data) throw new Error("Page Not Found")
   else redirect(url.data.longUrl)
 }
